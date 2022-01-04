dfa51ba0a48b564b2e5908dc3214be50b3d385af
MarekZhang <mark.bowen.zhang@gmail.com>
2022-01-04 17:01:52 +0000

    add body-parser and jwt

---
+++ src/configs/jwt.config.ts
@@ -0,0 +1,9 @@
+interface JWTConfig {
+  secret: string
+}
+
+const JWTConfig: JWTConfig = {
+  secret: 'jwt-secret-koa-typescrpit-boilerplalte',
+}
+
+export { JWTConfig }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import Koa from 'koa'
 import helmet from 'koa-helmet'
 import winston from 'winston'
+import cors from '@koa/cors'
+import jwt from 'koa-jwt'
+import bodyParser from 'koa-body'
 import router from './routes'
+import { JWTConfig } from './configs/jwt.config'
 
 const app = new Koa()
 
 app.use(helmet())
 
-app.use(router.routes())
+app.use(cors())
+
+app.use(bodyParser())
+
+app.use(
+  jwt({ secret: JWTConfig.secret }).unless({
+    path: [/\/register/, /\/signIn/],
+  })
+)
 
 app.use(router.routes())
 
